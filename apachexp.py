f5cbb442b732ce040715320df68b55736967e5b1
Mr.Mad <80827394+MadExploits@users.noreply.github.com>
2024-03-15 21:10:55 +0700

    Add files via upload

---
+++ apachexp.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+
+import urllib3
+import requests as req
+from os.path import exists
+from os import system, name
+from multiprocessing.dummy import Pool
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+class ApacheVuln:
+    def __init__(self):
+        self.list_version_apache = [
+            '2.4.49',
+            '2.4.50',
+            '2.4.51',
+            '2.4.52',
+            '2.4.53',
+            '2.4.54',
+        ]
+        self.user_agents = {
+            'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0"
+        }
+
+    def clear_command_line(self):
+        if name == "nt":
+            system("cls")
+        else:
+            system("clear")
+
+    def ApacheCheck(self,website):
+
+        with req.session() as s:
+            r = s.get(f'{website}', headers=self.user_agents, verify=False)
+            for x in range(len(self.list_version_apache)):
+                if f"Apache/{self.list_version_apache[x]}" in r.headers['Server']:
+                    print(f"{website} => Apache/{self.list_version_apache[x]}")
+                    with open('apachexp.txt', 'a+') as wr:
+                        wr.write(f"{website}" + "\n")
+                else:
+                    break
+                print(f"{website} => Cannot Find CVE-2021-41773 / CVE-2021-42013!")
+                    
+
+    def main(self):
+        
+        print("CVE-2021-41773 / CVE-2021-42013 Finder By ./Mr.Mad")
+
+        list_target = input("List of urls : ")
+        if exists(list_target):
+            with open(list_target, 'r') as r:
+                read = r.read()
+                target = read.split("\n")
+                with Pool(int(10)) as Th:
+                    Th.map(self.ApacheCheck, target)
+                    Th.join()
+                    Th.close()
+        else:
+            print(f"File Not Found [{list_target}]")
+
+
+
+
+if __name__ == "__main__":
+    run = ApacheVuln()
+    run.clear_command_line()
+    run.main()
